b6e29a304df032b87ab9118ebd7f96ce5b95b330
Mayukh Datta <mayukh5741@gmail.com>
2020-06-12 21:19:20 +0530

    removed zones

--- src/main/java/com/thecoducer/coronavirustracker/controllers/IndiaZonesController.java
+++ src/main/java/com/thecoducer/coronavirustracker/controllers/IndiaZonesController.java
@@ -10,19 +10,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import com.thecoducer.coronavirustracker.models.IndiaZones;
 import com.thecoducer.coronavirustracker.services.IndiaZonesDataService;
 
-@Controller
-public class IndiaZonesController {
-	
-	@Autowired
-	IndiaZonesDataService indiaZonesDataService;
-	
-	@GetMapping("/zones")
-	public String ZonesInfo(Model model) {
-		
-		List<IndiaZones> indiaZonesList = indiaZonesDataService.getIndiaZonesList();
-		
-		model.addAttribute("IndiaZonesList", indiaZonesList);
-		
-		return "zones";
-	}
-}
+//@Controller
+//public class IndiaZonesController {
+//	
+//	@Autowired
+//	IndiaZonesDataService indiaZonesDataService;
+//	
+//	@GetMapping("/zones")
+//	public String ZonesInfo(Model model) {
+//		
+//		List<IndiaZones> indiaZonesList = indiaZonesDataService.getIndiaZonesList();
+//		
+//		model.addAttribute("IndiaZonesList", indiaZonesList);
+//		
+//		return "zones";
+//	}
+//}
